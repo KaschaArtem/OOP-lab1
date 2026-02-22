fc1a559e17140aeea10173e14292cb4c9aacf5e3
KaschaArtem <kascha767611@gmail.com>
2026-02-22 13:08:28 +0300

    Added choise for topic of tweets

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,8 +1,64 @@
-﻿class Program {
+﻿using System;
+
+class Program {
     static void Main(string[] args) {
-        
-        var parser = new TweetParser();
-        var country = new Country(parser.GetStates());
-        
+
+        string GetDataPath() {
+            string? dir = Directory.GetCurrentDirectory();
+
+            while (!string.IsNullOrEmpty(dir)) {
+                var slnFiles = Directory.GetFiles(dir, "*.sln")
+                    .Concat(Directory.GetFiles(dir, "*.slnx"));
+                if (slnFiles.Any())
+                    return Path.Combine(dir, "data");
+
+                dir = Directory.GetParent(dir)?.FullName;
+            }
+
+            throw new DirectoryNotFoundException("No solution file found.");
+        }
+
+        string GetTopicFile(string dataPath) {
+            string topic = String.Empty;
+            string[] allFiles = Directory.GetFiles(dataPath);
+            List<string> tweetFileNames = new();
+
+            foreach (var file in allFiles) {
+                string fileName = Path.GetFileName(file);
+                if (fileName.Contains("_tweets") && fileName.Contains(".txt"))
+                    tweetFileNames.Add(fileName);
+            }
+
+            if (tweetFileNames.Count == 0) {
+                Console.WriteLine("Nothing to parse.");
+                return String.Empty;
+            }
+
+            Console.WriteLine("Choose file to parse:");
+            for (int i = 1; i <= tweetFileNames.Count; i++)
+                Console.WriteLine($"{i}) {tweetFileNames[i - 1]}");
+
+            string? input = Console.ReadLine();
+            try {
+                int number = int.Parse(input!);
+                topic = tweetFileNames[number - 1];
+            }
+            catch (Exception ex) {
+                Console.WriteLine(ex);
+                return String.Empty;
+            }
+
+            return topic;
+        }
+
+        string dataPath = GetDataPath();
+        var topicFile = GetTopicFile(dataPath);
+
+        if (topicFile == String.Empty)
+            return;
+
+        var parser = new TweetParser(dataPath);
+        var country = new Country(parser.GetStatesByTopicFile(topicFile));
+
     }
 }
--- Program/TweetParser.cs
+++ Program/TweetParser.cs
@@ -1,63 +1,15 @@
-public enum USAStateName
-{
-    AL,
-    AK,
-    AZ,
-    AR,
-    CA,
-    CO,
-    CT,
-    DE,
-    FL,
-    GA,
-    HI,
-    ID,
-    IL,
-    IN,
-    IA,
-    KS,
-    KY,
-    LA,
-    ME,
-    MD,
-    MA,
-    MI,
-    MN,
-    MS,
-    MO,
-    MT,
-    NE,
-    NV,
-    NH,
-    NJ,
-    NM,
-    NY,
-    NC,
-    ND,
-    OH,
-    OK,
-    OR,
-    PA,
-    RI,
-    SC,
-    SD,
-    TN,
-    TX,
-    UT,
-    VT,
-    VA,
-    WA,
-    WV,
-    WI,
-    WY
-}
-
 public class TweetParser {
-    
-    public TweetParser() {}
 
-    public List<State> GetStates() {
+    private string DataPath = String.Empty;
+
+    public TweetParser(string dataPath) {
+        DataPath = dataPath;
+    }
+
+    public List<State> GetStatesByTopicFile(string topicFile) {
         List<State> states = new List<State>();
+
+        
 
         return states;
     }
